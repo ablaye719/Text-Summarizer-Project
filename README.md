55deb624bd10b4da264aac80391a1b1c3ea69652
ablaye719 <sowablayesandiara@gmail.com>
2024-04-30 19:15:06 +0200

    Data ingestion notebook added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Text-Summarizer-Project
+# End to end Text-Summarizer-Project
+
+## Workflows
+
+1. Update config.yml
+2. Update params.yaml
+3. Update entity
+4. Update configuration manager in src config
+5. Update conponents
+6. Update pipeline
+7. Update the main.
+8. Update the app.py
--- template.py
+++ template.py
@@ -17,8 +17,8 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py"
     f"src/{project_name}/entity/__init__.py"
     f"src/{project_name}/constants/__init__.py"
-    "config/config.yml",
-    "params.yml",
+    "config/config.yaml",
+    "params.yaml",
     "app.py",
     "main.py",
     "Dockerfile",
